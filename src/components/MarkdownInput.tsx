2cc697ec62765277677cff5f1d353bccb34d01d4
sake <sakelog.website@gmail.com>
2022-04-15 06:57:39 +0900

    コンポーネント構造酒精

--- src/components/MarkdownInput.tsx
+++ src/components/MarkdownInput.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
 
-import { Flex, Box, Heading, Input, Textarea } from '@chakra-ui/react';
+import {
+  Flex,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Input,
+  Textarea,
+} from '@chakra-ui/react';
+import { markdownToHtml } from '../lib/markdownToHtml';
 
 const MarkdownInput = (props: MarkdownInput.Props) => {
   return (
     <Flex direction="column" h="100%">
-      <Box py={4}>
-        <Heading>題名</Heading>
+      <FormControl pb={4}>
+        <FormLabel htmlFor="markdownTitle">題名</FormLabel>
         <Input
+          id="markdownTitle"
           value={props.title}
           onChange={(e) => props.setTitle(e.target.value)}
           variant="filled"
           type="text"
         />
-      </Box>
-      <Box flex="1 1 0%" display="flex" flexDirection={'column'}>
-        <Heading>本文</Heading>
+        <FormHelperText>文章の題名を入力</FormHelperText>
+      </FormControl>
+      <FormControl flex="1" display="flex" flexDir="column">
+        <FormLabel htmlFor="markdownBody">本文</FormLabel>
         <Textarea
+          id="markdownBody"
           value={props.markdown}
           onChange={(e) => props.setMarkdown(e.target.value)}
           variant="filled"
@@ -24,7 +35,8 @@ const MarkdownInput = (props: MarkdownInput.Props) => {
           overflow="scroll"
           flex="1"
         />
-      </Box>
+        <FormHelperText>本文をマークダウン形式で入力</FormHelperText>
+      </FormControl>
     </Flex>
   );
 };
--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -7,15 +7,24 @@ import style from '../styles/Object/Component/preview.module.scss';
 
 const Preview = (props: Preview.Props) => {
   return (
-    <Flex h="100%" direction="column" overflow="scroll" px={4}>
-      <Box>
-        <Heading>{props.title}</Heading>
-      </Box>
-      <Box
-        dangerouslySetInnerHTML={{ __html: markdownToHtml(props.markdown) }}
-        className={style.body}
+    <Flex h="100%" direction="column">
+      <Box pb={2}>プレビュー</Box>
+      <Flex
+        direction="column"
         flex="1"
-      />
+        px={4}
+        py={2}
+        overflow="scroll"
+        border="2px"
+        borderColor="gray.200"
+        rounded="md"
+      >
+        <Heading>{props.title}</Heading>
+        <Box
+          dangerouslySetInnerHTML={{ __html: markdownToHtml(props.markdown) }}
+          className={style.body}
+        />
+      </Flex>
     </Flex>
   );
 };
